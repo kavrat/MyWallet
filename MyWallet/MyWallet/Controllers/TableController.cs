98355f6fc220fb188521b538d7ee0202e3e84f93
Alex <Alex@DESKTOP-D6G6JUL>
2017-10-29 17:23:08 +0200

    Added EF

---
+++ MyWallet/MyWallet/Controllers/TableController.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MyWallet.Controllers
+{
+    public class TableController : Controller
+    {
+        // GET: Table
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public PartialViewResult Filter(int? TypeId)
+        {
+
+        }
+
+        // GET: Table/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Table/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Table/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Table/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Table/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Table/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Table/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ MyWallet/MyWallet/Models/TableViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Web.Mvc;
+
+namespace MyWallet.Models
+{
+    public class TableViewModel
+    {
+        [Key]
+        public int Id { get; set; }
+        public DateTime Date { get; set; }
+        public double Amount { get; set; }
+        public string Description { get; set; }
+        public string NameOp { get; set; }
+        public int TypeId { get; set; }
+        public string OpType { get; set; }
+        public IEnumerable<SelectListItem> NameOpList { get; set; }
+        public int OpId { get; set; }
+    }
+}
