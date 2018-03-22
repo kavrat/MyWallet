3b993e539ba4ca11da3b1ddf6eecd7a35b9bbe8b
Kavrat <kostenko.alex.v@gmail.com>
2018-03-22 16:18:58 +0200

    EditFunction in Settings.js

--- MyWallet/MyWallet/vendor/settings/Settings.js
+++ MyWallet/MyWallet/vendor/settings/Settings.js
@@ -7,48 +7,62 @@
         var idSelect;
 
         if (btn.indexOf("Del") >= 0) {
-            var idType = Number(btnIds.charAt(0));
-            var idDel = Number(btnIds.substr(1));
-
-            if (idType === 1) {
-                idSelect = "#revData";
-            }
-            else {
-                idSelect = "#expData";
-            }
-
-            $.post("/settings/delete/", { index: idDel },
-                function () {
-                    $(idSelect).load("settings/typelist/", { type: idType });
-                });
+            idSelect = deleteFunc(btnIds, idSelect);
         }
 
         if (btn.indexOf("Add") >= 0) {
-            if (btn.indexOf("Rev") >= 0) {
-                TypesViewModel.Name = $("#revName").val();
-                TypesViewModel.TypeId = 1;
-                idSelect = "#revData";
+            idSelect = addFunc(btn, TypesViewModel, idSelect);
 
-                $("#revName").val(null);
-            }
-
-            else {
-                TypesViewModel.Name = $("#expName").val();
-                TypesViewModel.TypeId = 2;
-                idSelect = "#expData";
-
-                $("#expName").val(null);
-            }
-            $.post("/settings/add", TypesViewModel,
-                function () {
-                    $(idSelect).load('/settings/typelist/', { type: TypesViewModel.TypeId });
-                });
-            
         }
 
         if (btn.indexOf("Edit") >= 0) {
-
+            var temp = $(this).closest('tr');
+            editRow(temp);
         }
-        //idSelect = null;
     });
-})
+
+});
+
+function addFunc(btn, TypesViewModel, idSelect) {
+    if (btn.indexOf("Rev") >= 0) {
+        TypesViewModel.Name = $("#revName").val();
+        TypesViewModel.TypeId = 1;
+        idSelect = "#revData";
+        $("#revName").val(null);
+    }
+    else {
+        TypesViewModel.Name = $("#expName").val();
+        TypesViewModel.TypeId = 2;
+        idSelect = "#expData";
+        $("#expName").val(null);
+    }
+    $.post("/settings/add", TypesViewModel, function () {
+        $(idSelect).load('/settings/typelist/', { type: TypesViewModel.TypeId });
+    });
+    return idSelect;
+}
+
+function deleteFunc(btnIds, idSelect) {
+    var idType = Number(btnIds.charAt(0));
+    var idDel = Number(btnIds.substr(1));
+    if (idType === 1) {
+        idSelect = "#revData";
+    }
+    else {
+        idSelect = "#expData";
+    }
+    $.post("/settings/delete/", { index: idDel }, function () {
+        $(idSelect).load("settings/typelist/", { type: idType });
+    });
+    return idSelect;
+}
+
+function editRow(row) {
+    $('td:first-child', row).each(function () {
+        $(this).html('<input type="text" value="' + $(this).html() + '" />');
+    });
+    $('td:nth-child(2)', row).each(function () {
+        $(this).html('<button id="setSave"class="btn btn-primary" type="button">Save</button>');
+    });
+
+}
