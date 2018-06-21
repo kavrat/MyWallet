3759767ad6ced8393aa1f255df7951b76bc62e78
Kavrat <kostenko.alex.v@gmail.com>
2018-06-21 13:15:53 +0300

    Some refactoring

--- MyWallet/MyWallet/Controllers/HomeController.cs
+++ MyWallet/MyWallet/Controllers/HomeController.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Web;
 using System.Web.Mvc;
 using MyWallet.Models;
 
@@ -11,10 +10,7 @@ namespace MyWallet.Controllers
     {
         WalletDBEntities db = new WalletDBEntities();
         // GET: Home
-        public ActionResult Index()
-        {
-            return View();
-        }
+        public ActionResult Index() => View();
 
         public JsonResult GetMonthly()
         {
